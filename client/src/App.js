26a367c593acb701ae3069ff7bd0cb6b913d8b8e
zbass <smilejkh90@gmail.com>
2019-03-29 12:43:46 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -19,35 +19,20 @@ const styles = theme => ({
     minWidth: 1080
   }
 })
-const customers =[{
-  'regno': 1,
-  'id' : 'zbass',
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '지경호',
-  'birth': '123456',
-  'gender' : '여자',
-  'job' : '공무원'
-},
-{
-  'regno': 2,
-  'id' : 'hahori',
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '이윤성',
-  'birth': '454545',
-  'gender' : '남자',
-  'job' : '프리랜서'
-},
-{
-  'regno': 3,
-  'id' : 'hwikang',
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '강휘',
-  'birth': '987654',
-  'gender' : '남자',
-  'job' : '프로그래머'
-}
-]
 class App extends Component {
+  state={
+    customers:""
+  }
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
     const { classes} = this.props;
     return (
@@ -65,7 +50,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-             {customers.map(c=>{ return(<Customer key={c.regno} regno={c.regno}id={c.id}image={c.image}name={c.name}birth={c.birth} gender={c.gender} job={c.job}/>);})}
+             {this.state.customers ? this.state.customers.map(c=>{ return(<Customer key={c.regno} regno={c.regno}id={c.id}image={c.image}name={c.name}birth={c.birth} gender={c.gender} job={c.job}/>);
+             }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,7 +6,38 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req, res)=>{ //사용자가 hello로 접속할시 메시지를 띄운다.
-    res.send({message: 'Hello Express!'});
+// app.get('/api/hello', (req, res)=>{ //사용자가 hello로 접속할시 메시지를 띄운다.
+//     res.send({message: 'Hello Express!'});
+// });
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            'regno': 1,
+            'id' : 'zbass',
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '지경호',
+            'birth': '123456',
+            'gender' : '여자',
+            'job' : '공무원'
+          },
+          {
+            'regno': 2,
+            'id' : 'hahori',
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '이윤성',
+            'birth': '454545',
+            'gender' : '남자',
+            'job' : '프리랜서'
+          },
+          {
+            'regno': 3,
+            'id' : 'hwikang',
+            'image' : 'https://placeimg.com/64/64/3',
+            'name' : '강휘',
+            'birth': '987654',
+            'gender' : '남자',
+            'job' : '프로그래머'
+          }          
+    ]);
 });
 app.listen(port,()=> console.log(`Listening on port ${port}`)); //실로 앱을 동작시키는 것->동작되면 콘솔찍힘
