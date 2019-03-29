72b4742157234d5cbfa643aa7bee7213a9a1a621
zbass <smilejkh90@gmail.com>
2019-03-29 16:35:36 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 const styles = theme => ({
   root : {
     width: '100%',
@@ -17,13 +18,30 @@ const styles = theme => ({
   },
   table:{
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit*2
   }
 })
+/*
+  React의 컨포넌트 lifecycle
+  1. constructor()
+  2. componentWillMount() 컨포넌트가 마운트 되기전에 부름
+  3. render() 컨포넌트를 화면에 그림
+  4. componentDidMount()컨포넌트가 다 불러와졌을때 실행
+  비동기적 호출을 사용-> Api서버에서 응답을 하지 않으면 사용자에게 로딩화면을 출력해야함.
+
+  컨포넌트의 props or state => shouldComponentUpdate() 함수등이 사용되어 render를 다시 갱신함 
+  상태관리만 잘하면됨!!!
+*/
+
 class App extends Component {
   state={
-    customers:""
+    customers:"", //사용자 기본값 설정
+    completed: 0 //프로그래스바의 기본값 설정
   }
   componentDidMount(){
+    this.timer = setInterval(this.progress,20);
     this.callApi()
     .then(res => this.setState({customers:res}))
     .catch(err => console.log(err));
@@ -32,6 +50,10 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+  progress =()=>{
+    const{completed} = this.state;
+    this.setState({completed: completed>=100 ? 0: completed+1 });
   }
   render() {
     const { classes} = this.props;
@@ -51,7 +73,11 @@ class App extends Component {
           </TableHead>
           <TableBody>
              {this.state.customers ? this.state.customers.map(c=>{ return(<Customer key={c.regno} regno={c.regno}id={c.id}image={c.image}name={c.name}birth={c.birth} gender={c.gender} job={c.job}/>);
-             }) : ""}
+             }) : <TableRow>
+               <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+               </TableCell>
+             </TableRow>}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express= require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
